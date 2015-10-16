97aa89d8e6fb7abd5bb8def102e54e8572164b5e
Asus_Huang <hcpups0708@gmail.com>
2015-10-16 16:53:02 +0800

    add OneVsRestClassifier

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import random
 import operator
 from pprint import pprint
 from sklearn.naive_bayes import GaussianNB
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
 
 country={}
 ingredients={}
@@ -11,7 +13,8 @@ ingredients_total={}
 X=[]
 Y=[]
 X_test=[]
-gnb=GaussianNB()
+#gnb=GaussianNB()
+gnb=OneVsRestClassifier(LinearSVC(random_state=0))
 
 with open('train.json') as data_file:
     data = json.load(data_file)
@@ -22,7 +25,7 @@ with open('test.json') as data_file:
 
 
 subdata=data
-#subdata=random.sample(data,21000)
+#subdata=random.sample(data,13000)
 
 for dish in subdata:
     country[dish['cuisine']]=0
@@ -31,15 +34,15 @@ for dish in subdata:
         if ingredients_total.has_key(ing):
             ingredients_total[ing]+=1
         else:
-            ingredients_total[ing]=0
+            ingredients_total[ing]=1
 
 sorted_ingredients = sorted(ingredients_total.items(), key=operator.itemgetter(1))
-#pprint(sorted_ingredients)
+pprint(len(sorted_ingredients))
 
 useIngredients={}
 
 for ing in ingredients_total.keys():
-    if ingredients_total.get(ing)>10:
+    if ingredients_total.get(ing)>=0:
         useIngredients[ing]=0
 
 print(len(useIngredients))
@@ -61,10 +64,9 @@ for dish in test:
     X_test.append(attr.values())
 
 
-gnb.partial_fit(X,Y,country.keys())
+#gnb.partial_fit(X,Y,country.keys()) #gnb
+gnb.fit(X,Y) #one vs rest
 result=gnb.predict(X_test)
-print(result)
-#print(gnb.score(X,Y))
 
 out=open("ans.csv",'w')
 out.write("id,cuisine\n")
@@ -74,6 +76,10 @@ i=0
 for dish in test:
     out.write(str(dish['id'])+","+result[i]+"\n")
     i+=1
+
+
+print(result)
+print(gnb.score(X,Y))
 
 
 #pprint(data)
