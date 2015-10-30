6e582b55003b935a9f638e92dc869ba85f632450
cnt12 <cnt1347@gmail.com>
2015-10-30 14:51:07 +0800

    Revert "SpiceRate and feature importance"
    This reverts commit cc73116b2421de308f222d7d24607d788b22ae14.

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import json
 import io
 import random
 import multiprocessing as mp
-import re
-import os
 import operator
 from pprint import pprint
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
 from sklearn.naive_bayes import GaussianNB
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.multiclass import OneVsOneClassifier
@@ -31,36 +27,29 @@ def main():
     Y=[]
     X_unknown=[]
     #gnb=GaussianNB()
-    gnb=OneVsRestClassifier(LogisticRegression(),n_jobs=1)
+    gnb=OneVsRestClassifier(LogisticRegression(),n_jobs=2)
     #gnb=OneVsOneClassifier(LinearSVC(random_state=0))
     #gnb = AdaBoostClassifier(n_estimators=50)
     #gnb=GradientBoostingClassifier(verbose=2)
     #gnb=NearestCentroid(metric='euclidean')
     #gnb = KNeighborsClassifier(n_neighbors=1, algorithm = 'auto')
-    #gnb=RandomForestClassifier(verbose=1,n_jobs=2,min_samples_leaf=1,n_estimators=200,oob_score=1)
+    #gnb=RandomForestClassifier(verbose=1,n_jobs=20,min_samples_leaf=1,n_estimators=1000,oob_score=1)
 
     #load from json file
-    with io.open('train_cleaned.json', encoding = 'utf8') as data_file:
+    with io.open('train.json', encoding = 'utf8') as data_file:
         data = json.load(data_file)
 
-    with io.open('train_cleaned.json', encoding = 'utf8') as data_file:
+    with io.open('test.json', encoding = 'utf8') as data_file:
         test = json.load(data_file)
-
-    with io.open('spice.txt', encoding = 'utf8') as f:
-        spices = f.read().splitlines()
 
 
 
     subdata=data                        #use full data for training
     #subdata=random.sample(data,20)   #randomly select n data for training
 
-
-    #count and add ingredients / countries
+    #count and add ingredients
     for dish in subdata:
-        if dish['cuisine'] in country:
-            country[dish['cuisine']]+=1
-        else:
-            country[dish['cuisine']]=1
+        country[dish['cuisine']]=0
         for ing in dish['ingredients']:
             ingredients[ing]=0
             if ing in ingredients_total:
@@ -75,62 +64,35 @@ def main():
     useIngredients={}
 
     #feature selection
-    spice_total={}
-    spice_ings=[]
     for ing in ingredients_total.keys():
         if ingredients_total.get(ing)>=0:     #use the ingredients that appared more than n times as feature
             useIngredients[ing]=0
-        for word in ing.split():
-            if word in spices:
-                if word in spice_total:
-                    spice_total[word]+=1
-                else:
-                    spice_total[word]=1
-                if ing not in spice_ings:
-                    spice_ings.append(ing)
 
-    print(str(len(spice_total))+" spices found")
+    print(str(len(useIngredients))+" feature used")
 
     for dish in subdata:
         attr=useIngredients.copy()
-        attr['ingUsedInDish']=0
-        attr['spiceRate']=0.0
         country[dish['cuisine']]+=1;
         for ing in dish['ingredients']:
             if ing in ingredients_total:
                 attr[ing]+=1
-                attr['ingUsedInDish']+=1
-                if ing in spice_ings:
-                    attr['spiceRate']+=1
-        attr['spiceRate']/=attr['ingUsedInDish']
-        #print(dish['id'],attr['spiceRate'],attr['ingUsedInDish'])
         X.append(attr.values())
         Y.append(dish['cuisine'])
 
     for dish in test:
         attr=useIngredients.copy()
-        attr['ingUsedInDish']=0
-        attr['spiceRate']=0.0
         for ing in dish['ingredients']:
             if ing in ingredients_total:
                 attr[ing]+=1
-                attr['ingUsedInDish']+=1
-                if ing in spice_ings:
-                    attr['spiceRate']+=1
-        attr['spiceRate']/=attr['ingUsedInDish']
-        #print(dish['id'],attr['spiceRate'],attr['ingUsedInDish'])
         X_unknown.append(attr.values())
-    print(str(len(X[1]))+" features used")
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
     print("Start training")
-    #os.system("pause")
+
 
     gnb.fit(X_train,Y_train)
     #gnb.fit(X,Y)
-
-    #print(gnb.feature_importances_)
 
     print("Start predicting")
 
@@ -147,7 +109,7 @@ def main():
         out.write(str(dish['id'])+","+result[i]+"\n")
         i+=1
 
-    print(str(len(result))+" results")
+
     print(result)
 
     print("Scoring result")
