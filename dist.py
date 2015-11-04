cc812882ef2d066a102d8d511d665b83a69462ad
chalos <cyl_05019@hotmail.com>
2015-11-05 01:31:37 +0800

    two class finished

--- dist.py
+++ dist.py
@@ -1,38 +1,33 @@
 
 import json
 import io
+import time
 from sets import Set
-"""
-class baseWhatsCooking(object):
+
+class testWhatsCooking(object):
 	def __init__(self, fn):
 		self._fn = fn
 		self._filterWords = Set()
-		self._allIngredient = Set()
-		self._allCuisineType = Set()
 		self._filterWords_dirty = True
+
 		self.rawData = None
 
-		_load()
+		self._load()
 
 	""" PROTECTED METHOD (mechanism approach) """
 
 	def _load(self):
 		with io.open(self._fn, encoding = 'utf8') as data_file:
 			self.rawData = json.load(data_file)
+			data_file.close()
 
-		assert self.rawData not None
+		assert self.rawData is not None
 
 	def _parseRaw_getAllIngredientList(self):
 		for dish in self.rawData:
 			_ingredients = dish['ingredients']
 			_ingredients = map(self._ingredient_tokfilter, _ingredients)
 			self._allIngredient |= Set(_ingredients)
-	
-	def _parseRaw_getAllCuisineList(self):
-		for dish in self.rawData:
-			self._allCuisineType = dish['cuisine']
-	
-
 
 	def _ingredient_tokfilter(self, name):
 		tokens = name.split(' ')
@@ -58,161 +53,92 @@ class baseWhatsCooking(object):
 		assert not True
 
 	def getIngredientList(self):
-		if self._filterWord_dirty is True:
-			_parseRaw_getAllIngredientList()
+		if not hasattr(self, '_allIngredient'):
+			self._allIngredient = Set()
+			self._filterWords_dirty = True
+
+		if self._filterWords_dirty is True:
+			self._parseRaw_getAllIngredientList()
 			self._filterWords_dirty = False
 
-		assert self._filterWords_dirty is not True
+		assert self._filterWords_dirty is False
 
 		return list(self._allIngredient)
 
+	def dumpToFile(self, obj, extra_string = ''):
+		assert type(extra_string) is str
+		try:
+			timenow = time.time().replace('.', '_')
+			with io.open(self._fn + timenow + extra_string, encoding = 'utf8') as _file:
+				json.dump(obj, _file, indent=4, sort_keys=True)
+		except TypeError: raise
+		except OverflowError: raise
+		finally:
+			_file.close()
+
+
+class trainWhatsCooking(testWhatsCooking):
+	def __init__(self,fn):
+		testWhatsCooking.__init__(self, fn)
+		
+	""" PROTECTED METHOD (mechanism approach) """
+
+	def _parseRaw_getAllCuisineList(self):
+		for dish in self.rawData:
+			self._allCuisineType.add(dish['cuisine'])
+
+	def _parseRaw_getIngredientAsKey(self):
+		for dish in self.rawData:
+			assert dish['cuisine'] is not None and dish['ingredients'] is not None
+
+			cuisine_type = dish['cuisine']
+			ingredients = dish['ingredients']
+			for ing_name in ingredients:
+				ing_name = self._ingredient_tokfilter(ing_name)
+				
+				if ing_name in self._ingredientAsKey:
+					if cuisine_type in self._ingredientAsKey[ing_name]:
+						self._ingredientAsKey[ing_name][cuisine_type] += 1
+					else :
+						self._ingredientAsKey[ing_name][cuisine_type] = 1
+				else:
+					self._ingredientAsKey[ing_name] = {}
+					self._ingredientAsKey[ing_name][cuisine_type] = 1
+
+	""" PUBLIC METHOD (policy approach) """
+
 	def getCuisineList(self):
+		if not hasattr(self, '_allCuisineType'):
+			self._allCuisineType = Set()
+			self._parseRaw_getAllCuisineList()
+
 		return list(self._allCuisineType)
 
-	def dump():
-		pass
+	def getIngredientAsKey(self):
+		if not hasattr(self, '_ingredientAsKey'):
+			self._ingredientAsKey = {}
+			self._filterWords_dirty = True
 
-"""
+		if self._filterWords_dirty is True:
+			self._parseRaw_getIngredientAsKey()
+			self._filterWords_dirty = False
 
-filter_words = [
-	'oz', 'lb', 'le', ''
-]
-
-def getIngredientList(fn, f_filter=None):
-	""" Get list of ingredients available in test.json/train.json
-	Args:
-		fn (str): file location of test.json
-		f_filter (func): function of filter of ingredient words
-			def f_filter(params):
-				Args: 
-					str: string of ingredients 
-				Returns:
-					str: filtered string of ingredients
-	Returns:
-		list: result of test.json, filtered
-	Examples:
-		To 
-		>>> getIngredientList('test_cleaned.json')
-		["A Taste Thai Rice Noodles", "Accent Seasoning", ...]
-		
-		>>> def ingredient_name_process(in):
-		>>>		return in.replace(' ', '_')
-		>>> ingredientList = getIngredientList('test_cleaned.json', ingredient_name_process)
-		["A_Taste_Thai_Rice_Noodles", "Accent_Seasoning", ...]
-	"""
-	assert type(fn) is str
-	assert hasattr(ing_filter, '__call__')
-
-	data = None
-	total_ingredient_test = Set()
-
-	with io.open(fn, encoding = 'utf8') as data_file:
-		data = json.load(data_file)
-
-	assert data is not None
-
-	for dish in data:
-		_ingredients = dish['ingredients']
-		_ingredients = map(lambda x: f_filter(x), _ingredients)
-		total_ingredient_set |= Set(_ingredients)
-
-	return list(total_ingredient_test)
-
-def ingredientAsKey(fn, f_filter=None, l_filter:None):
-	""" Get list of ingredients available in train.json
-	Args:
-		fn (str): file location of test.json
-		f_filter (func): function of filter, refer to @getIngredientList(fn,f_filter)
-		l_filter (func): function of filter of ingredient array
-			def l_filter(params):
-				Args: 
-					list: ingredients 
-				Returns:
-					list: filtered ingredients list
-	Returns:
-		dict: ingredient_cuisine_distribution
-	Examples:
-		>>> ingredientAsKey('train_cleaned.json')
-
-	"""
-	assert type(fn) is str
-	assert hasattr(ffilter, '__call__')
-
-	data = None
-	ingredient_cuisine_dist={}
-
-	with io.open(fn, encoding = 'utf8') as data_file:
-		data = json.load(data_file)
-
-	assert data is not None and type(data) is list
-
-	for dish in data:
-		assert dish['cuisine'] is not None and dish['ingredients'] is not None
-
-		ingredients = dish['ingredients']
-		for ing_name in ingredients:
-			ing_name = f_filter(ing_name)
-			
-			if ing_name in ingredient_cuisine_dist:
-				if cuisine_type in ingredient_cuisine_dist[ing_name]:
-					ingredient_cuisine_dist[ing_name][cuisine_type] += 1
-				else :
-					ingredient_cuisine_dist[ing_name][cuisine_type] = 1
-			else:
-				ingredient_cuisine_dist[ing_name] = {}
-				ingredient_cuisine_dist[ing_name][cuisine_type] = 1
-
-	return ingredient_cuisine_dist
-
-def ingredient_name_process(name):
-	global filter_words
-
-	tokens = name.split(' ')
-	
-	for fword in filter_words:
-		tokens = filter(lambda x: x!=fword, tokens)
-
-	joined = ' '.join(tokens)
-	return joined
+		return self._ingredientAsKey
 
 def main():
-	testIngredientList = getTestIngredientList('test_cleaned.json', ingredient_name_process)
-	
-	ingredient_cuisine_dist={}
-	total_ingredient_freq = {}
-	total_cuisine_freq = {}
+	filter_words = [
+		'oz', 'lb', 'le', ''
+	]
 
-	with io.open('train_cleaned.json', encoding = 'utf8') as data_file:
-		data = json.load(data_file)
+	test = testWhatsCooking('test_cleaned.json')
+	test.addFilterWords(filter_words)
+	#print test.getIngredientList()
 
-	for dish in data:
-		cuisine_type = dish['cuisine']
-		if cuisine_type in total_cuisine_freq:
-			total_cuisine_freq[cuisine_type] += 1
-		else:
-			total_cuisine_freq[cuisine_type] = 1
-
-		ingredients = dish['ingredients']
-		for ing_name in ingredients:
-			ing_name = ingredient_name_process(ing_name)
-			if ing_name in total_ingredient_freq:
-				total_ingredient_freq[ing_name] += 1
-			else:
-				total_ingredient_freq[ing_name] = 1
-
-			if ing_name in ingredient_cuisine_dist:
-				if cuisine_type in ingredient_cuisine_dist[ing_name]:
-					ingredient_cuisine_dist[ing_name][cuisine_type] += 1
-				else :
-					ingredient_cuisine_dist[ing_name][cuisine_type] = 1
-			else:
-				ingredient_cuisine_dist[ing_name] = {}
-				ingredient_cuisine_dist[ing_name][cuisine_type] = 1
-
-	#print json.dumps(ingredient_cuisine_dist, indent=4, sort_keys=True)
-	#print "length of total ingredients", len(total_ingredient_freq)
-	print json.dumps(total_cuisine_freq, indent=4, sort_keys=True)
-
+	train = trainWhatsCooking('train_cleaned.json')
+	train.addFilterWords(filter_words)
+	#print train.getIngredientList()
+	print train.getCuisineList()
+	print train.getIngredientAsKey()
 
 if __name__=='__main__':
 	main()
